77b6af5a74615e381fddec70d27761a03bafab8f
Mehdi Bagheri <mehdibaghery@gmail.com>
2023-05-21 19:46:25 +0200

    dht22 sensor

---
+++ dht22.py
@@ -0,0 +1,19 @@
+import time
+
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+
+dht22_pin = 26
+
+GPIO.setmode(GPIO.BCM)
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(22, dht22_pin)
+
+    if humidity is not None and temperature is not None:
+        print(f'Temperature: {temperature:.2f} C')
+        print(f'Humidity: {humidity:.2f} %')
+    else:
+        print('Failed to read data from DHT11 sensor')
+
+    time.sleep(5)
